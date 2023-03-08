b101210db062840f95a085a002ba4712b3b006eb
Byansi Anthony <101401469+AnthonyByansi@users.noreply.github.com>
2023-03-08 22:53:21 +1300

    Update hello-world.ino

---
+++ SpeakUp-BCI/examples/hello-world/hello-world.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+  display.display();
+}
+
+void loop() {
+  // Do nothing
+}
